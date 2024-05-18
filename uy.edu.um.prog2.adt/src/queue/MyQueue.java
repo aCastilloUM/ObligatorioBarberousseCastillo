2b80d89219f404e9102f1c1097f77d7e9d5f73ba
Lbarberousse <lbarberousse@correo.um.edu.uy>
2024-05-18 15:50:55 -0300

    Actualizacion 1 Queue, enqueue, dequeue, isEmpty, printQueue

--- uy.edu.um.prog2.adt/src/queue/MyQueue.java
+++ uy.edu.um.prog2.adt/src/queue/MyQueue.java
@@ -1,4 +1,8 @@
 package queue;
 
 public interface MyQueue<T> {
+    boolean isEmpty();
+    void enqueue(T valor);
+    void dequeue() throws EmptyQueueException;
+    void printQueue();
 }
--- uy.edu.um.prog2.adt/src/queue/Queue.java
+++ uy.edu.um.prog2.adt/src/queue/Queue.java
@@ -1,4 +1,56 @@
 package queue;
 
 public class Queue<T> implements MyQueue<T> {
+
+    QueueNode<T> first;
+    QueueNode<T> last;
+    int size;
+
+    public Queue(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (first == null && size == 0);
+    }
+
+    @Override
+    public void enqueue(T valor) {
+        QueueNode<T> newQueue = new QueueNode<T>(valor);
+        if (last == null){
+            last = newQueue;
+        } else {
+            QueueNode<T> temp = last;
+            while (temp.getPrevious() != null){
+                temp = temp.getPrevious();
+            }
+            temp.setPrevious(newQueue);
+            first = newQueue;
+        }
+    }
+
+    @Override
+    public void dequeue() throws EmptyQueueException {
+        QueueNode<T> temp = last;
+        if (isEmpty()){
+            throw new EmptyQueueException();
+        }
+        while (temp.getPrevious().getPrevious() != null){
+            temp = temp.getPrevious();
+        }
+        first = temp;
+        first.setPrevious(null);
+    }
+
+    @Override
+    public void printQueue() {
+        QueueNode<T> temp = last;
+        while (temp != null){
+            System.out.println(temp.getValue());
+            temp = temp.getPrevious();
+        }
+    }
 }
