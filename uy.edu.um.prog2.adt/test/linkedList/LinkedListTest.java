a21885a3b1925fe5fee4cd988de59cb547d81ef2
Lbarberousse <lbarberousse@correo.um.edu.uy>
2024-05-20 11:09:12 -0300

    Test unitario 2

--- uy.edu.um.prog2.adt/test/linkedList/LinkedListTest.java
+++ uy.edu.um.prog2.adt/test/linkedList/LinkedListTest.java
@@ -6,19 +6,54 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LinkedListTest {
     @Test
-    public void addTest() throws EmptyListException {
+    public void addFirstTest() {
         LinkedList<String> lista = new LinkedList<>();
         lista.addFirst("Hola");
         lista.addFirst("Mundo");
         lista.addFirst("Chau");
-
         assertEquals(3, lista.size);
+        assertEquals("Chau", lista.getValueNode(0));
+    }
+
+    @Test
+    public void addLastTest() {
+        LinkedList<String> lista = new LinkedList<>();
+        lista.addLast("Hola");
+        lista.addLast("Mundo");
+        lista.addLast("Chau");
+        assertEquals(3, lista.size);
+        assertEquals("Hola", lista.getValueNode(0));
+    }
+
+    @Test
+    public void removeTest() throws EmptyListException {
+        LinkedList<String> lista = new LinkedList<>();
+        lista.addFirst("Hola");
+        lista.addFirst("Mundo");
+        lista.addFirst("Chau");
 
         lista.remove(0);
 
         assertEquals(2, lista.size);
         assertEquals("Mundo", lista.getValueNode(0));
 
+        lista.remove(0);
+        lista.remove(0);
+        assertThrows(EmptyListException.class, () -> {
+            lista.remove(0);
+        });
     }
+
+    @Test
+    public void containsTest (){
+        LinkedList<String> lista = new LinkedList<>();
+        lista.addFirst("Hola");
+        lista.addFirst("Mundo");
+        lista.addLast("Chau");
+        lista.addFirst("Perro");
+        lista.addLast("Gato");
+        assertTrue(lista.contains("Perro"));
+    }
+
 
 }
