3dfc74e814fb4f90b7dc9ee77bf102f86a0bda81
Lbarberousse <lbarberousse@correo.um.edu.uy>
2024-05-27 16:23:10 -0300

    Creacion readerCSV

---
+++ uy.edu.um.prog2.adt/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        readeCSV file = new readeCSV();
+        file.readFile("C:\\Users\\Lu\\Documents\\UM2024\\Programacion II\\universal_top_spotify_songs.csv");
+    }
+}
