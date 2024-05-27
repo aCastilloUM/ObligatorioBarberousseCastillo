f2a18b272f86a355e7488eab6e933cf49f433235
Lbarberousse <lbarberousse@correo.um.edu.uy>
2024-05-27 16:39:29 -0300

    Actualizacion readerCSV

---
+++ uy.edu.um.prog2.adt/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        readerCSV file = new readerCSV();
+        file.readFile("C:\\Users\\Lu\\Documents\\UM2024\\Programacion II\\universal_top_spotify_songs.csv");
+    }
+}
---
+++ uy.edu.um.prog2.adt/src/readerCSV.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class readerCSV {
+    public BufferedReader reader;
+    public String line;
+    public String parts[];
+
+    public void readFile(String file_name) {
+        try {
+
+            reader = new BufferedReader(new FileReader(file_name));
+            while ((line = reader.readLine()) != null){
+                parts = line.split(" ; " );
+                printLine();
+                System.out.println();
+            }
+            reader.close();
+            line = null;
+            parts = null;
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+
+    public void printLine(){
+        for (int i = 0; i < parts.length; i++){
+            System.out.println(parts[i] + "    |   ");
+        }
+    }
+}
