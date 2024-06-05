5feb2ca3dd37ac53d32cd25b0c46931986da5fc1
aCastilloUM <a.castillo@correo.um.edu.uy>
2024-06-05 13:19:14 -0300

    Commiteo para que Lu siga

--- uy.edu.um.prog2.adt/src/ObligatoryMethodsImpl.java
+++ uy.edu.um.prog2.adt/src/ObligatoryMethodsImpl.java
@@ -30,11 +30,10 @@ public class ObligatoryMethodsImpl implements ObligatoryMethods{
             System.out.println((i+1) + " - " + s.getName() + "    " + s.getArtists());
         }
     }
-
+    LinkedList<ListNode<String>> dateSongs = new LinkedList<>();
     public void top5RepeatedSongs(String date){
 
         int counter = 0;
-        LinkedList<ListNode<String>> dateSongs = new LinkedList<>();
         ListNode<String> firstNotLinkedNode = null;
 
         for (int i = 0 ; i<abbreviations.length; i++){
@@ -46,29 +45,41 @@ public class ObligatoryMethodsImpl implements ObligatoryMethods{
                 if (counter == 1){
                     dateSongs.addFirst(file.getWorld().get(key).getHead());
                     enlazarNodosRecursivo(dateSongs.getHead().getValue(),file.getWorld().get(key).getHead().getNext());
+                    System.out.println(dateSongs.getSize());
+                    dateSongs.printList();
                 }
                 else{
-                    firstNotLinkedNode = file.getWorld().get(key).getHead();
-                    dateSongs.addLast(firstNotLinkedNode);
+                    getTail(dateSongs.getHead().getValue()).setNext(file.getWorld().get(key).getHead());
+                    dateSongs.printList();
+                    System.out.println(dateSongs.getSize());
+                    enlazarNodosRecursivo(dateSongs.getLast().getValue(), file.getWorld().get(key).getHead());
                 }
             } catch (InvalidKeyException | exceptions.EmptyHashException EmptyHashException ){
                 System.out.println("No hay pais");
             }
         }
         //Veamos q se mantenga
-        System.out.println(dateSongs.getHead().getValue().getValue());
-        System.out.println(dateSongs.getHead().getNext().getNext().getNext().getValue().getValue());
+        System.out.println(dateSongs.getSize());
     }
 
-    public static void enlazarNodosRecursivo(ListNode<String> a, ListNode<String> b) {
-        if (a == null || b.getNext() == null)
+    public void enlazarNodosRecursivo(ListNode<String> a, ListNode<String> b) {
+        if (b == null || b.getNext() == null) // Verificar si b o su siguiente nodo son nulos
             return;
 
+        this.dateSongs.addLast(b);
         a.setNext(b);
-        enlazarNodosRecursivo(a, b.getNext());
+        enlazarNodosRecursivo(b, b.getNext());
     }
 
+    public static ListNode<String> getTail(ListNode<String> head) {
+        // Caso base: si la cabeza es nula o su siguiente nodo es nulo, la cola también será nula
+        if (head == null || head.getNext() == null) {
+            return head;
+        }
 
+        // Caso recursivo: llamar al método con el siguiente nodo
+        return getTail(head.getNext());
+    }
     String[] abbreviations = {"GLB","ZA"};
 
 }
